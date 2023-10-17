5baa9acba44ca0bec90165e1f19b504aaec64fb4
Bori <boriakinola01@gmail.com>
2023-10-17 17:10:54 +0100

    Initial Commit

---
+++ CalcSwiftUI/CalcSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcSwiftUIApp.swift
+//  CalcSwiftUI
+//
+//  Created by Bori Akinola on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalcSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
